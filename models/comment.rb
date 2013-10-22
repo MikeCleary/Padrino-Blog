0959938bcd267cd8205846b511229531a15eba00
Michael <Cleary481@gmail.com>
2013-10-22 18:00:08 +0100

    comment test passes

--- models/comment.rb
+++ models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  has_many :replies, :class_name => 'Comment'
+  has_many :replies, :class_name => 'Comment', :foreign_key => :parent_id
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,20 +3,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 class CommentTest < Test::Unit::TestCase
   context "Comment Model" do
     setup do
-      @comment = Comment.new(:content => "What a great post", :post_date => Time.now)
-      @post << @comment
+      @comment = Comment.new(:content => "What a great post", :comment_date => Time.now)
+      
+      @post = Post.new(:title => "My amazing blog", :post_date => Time.now)
+      @post.comments << @comment
       @post.save!
       @post.reload
     end
 
     should 'save to the database correctly' do
-      assert_equal "What a great post", comment.content
+      assert_equal "What a great post", @comment.content
       assert_equal @post, @comment.post
     end
 
     context "replies" do
       setup do
-        @reply = Comment.new(:content => "ur Hitler", :post_date => Time.now)
+        @reply = Comment.new(:content => "ur Hitler", :comment_date => Time.now)
         @comment.replies << @reply 
         @comment.save!
         @comment.reload
