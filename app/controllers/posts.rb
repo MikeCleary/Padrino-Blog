5edb0e47707275f77ee4ff445f1f585bab30130b
Michael <Cleary481@gmail.com>
2013-10-23 14:15:40 +0100

    index page built. all posts test passed

---
+++ app/controllers/posts.rb
@@ -0,0 +1,52 @@
+PadrinoBlog::App.controllers :posts do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+  get :index, :map => '/posts' do
+    @posts = Post.all
+    render 'posts/index'
+  end
+
+  get :show do
+
+  end
+
+  get :new do
+    @post = Post.new
+    erb :new
+  end
+
+  post :create do
+
+  end
+
+  get :edit do
+
+  end
+
+  put :update do
+
+  end
+
+  delete :destroy do
+
+  end
+
+end
---
+++ test/app/controllers/posts_controller_test.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../test_config.rb')
+
+class PostsControllerTest < Test::Unit::TestCase
+  context "PostsController" do
+    setup do
+      3.times {Post.create(:title => 'My Amazing Post') }
+
+      get '/posts'
+    end
+
+    should "return Posts text" do
+      assert_match "All Posts", last_response.body
+      assert_match "My Amazing Post", last_response.body
+    end
+  end
+end
